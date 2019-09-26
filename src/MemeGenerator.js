393e419e3946a5efd219517c551107547abea0a5
Crystal Fong <crystal396@hotmail.com>
2019-09-26 16:54:36 +0100

    Add files via upload

---
+++ src/MemeGenerator.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react"
+
+export default MemeGenerator
+
+class MemeGenerator extends Component(){
+    constructor(){
+        super()
+        this.state= {}
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Main meme generator</h1>
+            </div>
+        )
+    }
+}
